50d1c8c25a1f68a66856815327239665c6b3ce58
Delon Newman <delon.newman@gmail.com>
2012-01-30 18:55:04 -1000

    working on stacking pieces in grid

--- tetris.js
+++ tetris.js
@@ -6,49 +6,49 @@
 var tetris = { VERSION : '0.0.0' };
 
 tetris.config = function(){
-	var config = {
-		SQUARE_PX : 25,
-		SPACING   : 0,
+    var config = {
+        SQUARE_PX : 25,
+        SPACING   : 0,
 
-		COLORS : {
-			'I' : "#ff0000",
-			'J' : "#00ff00",
-			'L' : "#0000ff",
-			'O' : "#ffff00",
-			'S' : "#ff00ff",
-			'T' : "#ffffff",
-			'Z' : "#00ffff"
-		},
+        COLORS : {
+            'I' : "#ff0000",
+            'J' : "#00ff00",
+            'L' : "#0000ff",
+            'O' : "#ffff00",
+            'S' : "#ff00ff",
+            'T' : "#ffffff",
+            'Z' : "#00ffff"
+        },
 
-		// drop rates for each level
-		RATES : {
-			1 : 30,
-			2 : 20,
-			3 : 10,
-			4 : 0.8,
-			5 : 0.6,
-			6 : 0.4,
-			7 : 0.2
-		},
+        // drop rates for each level
+        RATES : {
+            1 : 30,
+            2 : 20,
+            3 : 10,
+            4 : 0.8,
+            5 : 0.6,
+            6 : 0.4,
+            7 : 0.2
+        },
 
-		WIDTH  : 20,
-		HEIGHT : 20
-	};
+        WIDTH  : 20,
+        HEIGHT : 20
+    };
 
-	config.pixelSize = function(dimension) {
-		return dimension * (config.SQUARE_PX + config.SPACING);
-	};
+    config.pixelSize = function(dimension) {
+        return dimension * (config.SQUARE_PX + config.SPACING);
+    };
 
-	config.gridSize = function(dimension) {
-		return Math.floor(dimension / (config.SQUARE_PX + config.SPACING));
-	};
+    config.gridSize = function(dimension) {
+        return Math.floor(dimension / (config.SQUARE_PX + config.SPACING));
+    };
 
-	config.PIXEL_WIDTH  = config.pixelSize(config.WIDTH);
-	config.PIXEL_HEIGHT = config.pixelSize(config.HEIGHT);
+    config.PIXEL_WIDTH  = config.pixelSize(config.WIDTH);
+    config.PIXEL_HEIGHT = config.pixelSize(config.HEIGHT);
 
-	config.INIT_POSITION = [config.pixelSize(Math.round(config.WIDTH / 2)), 0];
+    config.INIT_POSITION = [config.pixelSize(Math.round(config.WIDTH / 2)), 0];
 
-	return config;
+    return config;
 }();
 
 tetris.model = function(){
@@ -126,8 +126,8 @@ tetris.model = function(){
 
             var that = this;
             setTimeout(function(){
-			if ( !that.isPaused() && _.position[1] < 
-				(tetris.config.PIXEL_HEIGHT - that.pixelWidth()) ) {
+            if ( !that.isPaused() && _.position[1] < 
+                (tetris.config.PIXEL_HEIGHT - that.pixelWidth()) ) {
                     _.position[1]++;
                     that.drop(rate);
                 }
@@ -151,97 +151,97 @@ tetris.model = function(){
         };
 
         this.position = function(val) {
-			if ( val ) {
-				// NOTE: we want a clean copy
-				this._.position    = [];
-				this._.position[0] = val[0];
-				this._.position[1] = val[1];
-			}
+            if ( val ) {
+                // NOTE: we want a clean copy
+                this._.position    = [];
+                this._.position[0] = val[0];
+                this._.position[1] = val[1];
+            }
             else { return this._.position }
         };
 
-		this.gridPosition = function() {
-			var pixels = this.position();
-			var grid   = [ tetris.config.gridSize(pixels[0]) - 1,
-							tetris.config.gridSize(pixels[1]) - 1 ];
+        this.gridPosition = function() {
+            var pixels = this.position();
+            var grid   = [ tetris.config.gridSize(pixels[0]) - 1,
+                            tetris.config.gridSize(pixels[1]) - 1 ];
 
-			return grid;
-		};
+            return grid;
+        };
 
         this.move = function(direction, degree, test) {
             var p = this._.position[direction];
 
             if ( test.call(this, p) ) {
                 this._.position[direction] = p + degree;
-				console.log(this._.position);
+                console.log(this._.position);
                 return true;
             }
             return false;
         };
 
         this.width = function() {
-			var matrix = this.matrix();
+            var matrix = this.matrix();
 
-			var sizes = [];
+            var sizes = [];
 
-			var i = 0;
-			for ( ; i < matrix.length; i++ ) {
-				var j = 0;
-				var rowSize = 0;
-				for ( ; j < matrix[i].length; j++ ) {
-					if ( matrix[i][j] != 0 ) rowSize++;
-				}
-				sizes.push(rowSize);
-			}
+            var i = 0;
+            for ( ; i < matrix.length; i++ ) {
+                var j = 0;
+                var rowSize = 0;
+                for ( ; j < matrix[i].length; j++ ) {
+                    if ( matrix[i][j] != 0 ) rowSize++;
+                }
+                sizes.push(rowSize);
+            }
 
-			return sizes.sort().pop();
-		};
+            return sizes.sort().pop();
+        };
 
-		this.pixelWidth = function() {
-			return tetris.config.pixelSize(this.width());
+        this.pixelWidth = function() {
+            return tetris.config.pixelSize(this.width());
         };
 
         this.height = function() {
-			var matrix = this.matrix();
+            var matrix = this.matrix();
 
-			var sizes = [];
+            var sizes = [];
 
-			var i = 0;
-			for ( ; i < matrix.length; i++ ) {
-				var j = 0;
-				var colSize = 0;
-				for ( ; j < matrix[i].length; j++ ) {
-					if ( matrix[j][i] != 0 ) colSize++;
-				}
-				sizes.push(colSize);
-			}
+            var i = 0;
+            for ( ; i < matrix.length; i++ ) {
+                var j = 0;
+                var colSize = 0;
+                for ( ; j < matrix[i].length; j++ ) {
+                    if ( matrix[j][i] != 0 ) colSize++;
+                }
+                sizes.push(colSize);
+            }
 
-			return sizes.sort().pop();
-		};
+            return sizes.sort().pop();
+        };
 
-		this.pixelHeight = function(){
-			return tetris.config.pixelSize(this.height());
+        this.pixelHeight = function(){
+            return tetris.config.pixelSize(this.height());
         };
 
         this.moveRight = function() {
             console.log("move right");
-			var space = tetris.config.SQUARE_PX + (tetris.config.SPACING * 2);
-			return this.move(0, space, function(p){
-				return p < (tetris.config.PIXEL_WIDTH - this.pixelWidth())
-			});
+            var space = tetris.config.SQUARE_PX + (tetris.config.SPACING * 2);
+            return this.move(0, space, function(p){
+                return p < (tetris.config.PIXEL_WIDTH - this.pixelWidth())
+            });
         };
 
         this.moveLeft = function() {
             console.log("move left");
-			var space = -(tetris.config.SQUARE_PX + (tetris.config.SPACING * 2));
-			return this.move(0, space, function(p){ return p > 0 });
+            var space = -(tetris.config.SQUARE_PX + (tetris.config.SPACING * 2));
+            return this.move(0, space, function(p){ return p > 0 });
         };
 
         this.moveDown = function() {
             console.log("move down");
-			return this.move(1, tetris.config.SQUARE_PX, function(p){
-				return p < (tetris.config.PIXEL_HEIGHT - this.pixelHeight())
-			});
+            return this.move(1, tetris.config.SQUARE_PX, function(p){
+                return p < (tetris.config.PIXEL_HEIGHT - this.pixelHeight())
+            });
         };
 
         this.rotate = function() {
@@ -296,10 +296,10 @@ tetris.model = function(){
     function Grid(width, height) {
         this.width  = width;
         this.height = height;
-		this._      = {};
+        this._      = {};
 
-		// private scope
-		var _  = this._;
+        // private scope
+        var _  = this._;
         _.grid = function (w, h) {
             var b = [];
             var i = 0;
@@ -313,9 +313,9 @@ tetris.model = function(){
             return b;
         }(this.width, this.height);
 
-		this.grid = function(){
-			return this._.grid;
-		};
+        this.grid = function(){
+            return this._.grid;
+        };
 
         this.display = function() {
             var i = 0;
@@ -331,22 +331,22 @@ tetris.model = function(){
             var matrix = t.matrix();
             var pos    = t.gridPosition();
 
-			console.log(pos);
-			console.log(t.width());
-			console.log(t.height());
+            console.log(pos);
+            console.log(t.width());
+            console.log(t.height());
 
             var i = 0;
             for ( ; i < matrix.length; i++ ) {
-				var j = 0;
-				var x = pos[0] + i;
-				this.grid()[x] = [];
-				for ( ; j < matrix[i].length; j ++ ) {
-					var y = pos[1] + j;
-					this.grid()[x][y] = matrix[j][i];
-				}
+                var j = 0;
+                var x = pos[0] + i;
+                this.grid()[x] = [];
+                for ( ; j < matrix[i].length; j ++ ) {
+                    var y = pos[1] + j;
+                    this.grid()[x][y] = matrix[j][i];
+                }
             }
 
-			this.display();
+            this.display();
         };
     }
 
@@ -372,7 +372,7 @@ tetris.view = function ( $ ) {
         this.queue = new tetris.model.Queue();
         this.currentTetrimino = this.queue.dequeue();
         this.currentTetrimino.position(tetris.config.INIT_POSITION);
-		console.log(this.currentTetrimino.position());
+        console.log(this.currentTetrimino.position());
 
         this.onFallen = function f(t) {
             console.log(this);
@@ -418,18 +418,18 @@ tetris.view = function ( $ ) {
             }
         };
 
-		this.renderGrid = function(g) {
+        this.renderGrid = function(g) {
             var matrix = g.grid();
 
             var i = 0;
             for ( ; i < matrix.length; i++ ) {
                 var j = 0;
-				if ( matrix[i] ) {
-					for ( ; j < matrix[i].length; j++ ) {
-						if ( !!matrix[i][j] )
-							this.renderSquare(tetris.config.pixelSize(i), tetris.config.pixelSize(j));
-					}
-				}
+                if ( matrix[i] ) {
+                    for ( ; j < matrix[i].length; j++ ) {
+                        if ( !!matrix[i][j] )
+                            this.renderSquare(tetris.config.pixelSize(i), tetris.config.pixelSize(j));
+                    }
+                }
             }
         };
 
@@ -437,7 +437,7 @@ tetris.view = function ( $ ) {
             this.context.fillStyle = "#000000";
             this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
             this.renderTetrimino(this.currentTetrimino);
-			this.renderGrid(this.grid);
+            this.renderGrid(this.grid);
         };
 
         this.start = function() {
@@ -453,7 +453,7 @@ tetris.view = function ( $ ) {
 
         this.quit = function() { };
 
-		this.render();
+        this.render();
     }
 
     return { Game:Game };
